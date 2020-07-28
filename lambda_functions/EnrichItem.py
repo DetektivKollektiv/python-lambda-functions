ee84a2e78c4fedc03ee704c89159768952aa14ad
ML-Dirk <mail@drwacker.de>
2020-07-29 01:10:16 +0200

    store item urls in DB

--- lambda_functions/EnrichItem.py
+++ lambda_functions/EnrichItem.py
@@ -1,5 +1,5 @@
 import logging
-from crud.model import Item, ExternalFactCheck, ItemURL, FactChecking_Organization
+from crud.model import Item, ExternalFactCheck, ItemURL, URL, FactChecking_Organization
 from crud import operations
 from uuid import uuid4
 
@@ -83,7 +83,6 @@ def store_factchecks(event, context):
         else:
             org_name = "Unknown"
         # Does the publishing organization already exist?
-        # search for url in database
         try:
             organization = operations.get_organization_by_content_db(org_name)
         except Exception:
@@ -96,10 +95,18 @@ def store_factchecks(event, context):
                 operations.update_object_db(organization)
             except Exception as e:
                 logger.error("Could not store Organization. Exception: %s", e, exc_info=True)
+
+        factcheck_url = json_event['claimReview'][0]['url']
+        item_id = event['item']['id']
+        try:
+            # Does the factcheck already exist?
+            factcheck = operations.get_factcheck_by_url_and_item_db(factcheck_url, item_id)
+        except Exception as e:
+            # create new factcheck in database
+            factcheck.id = str(uuid4())
         # store factcheck in database
-        factcheck.id = str(uuid4())
-        factcheck.url = json_event['claimReview'][0]['url']
-        factcheck.item_id = event['item']['id']
+        factcheck.url = factcheck_url
+        factcheck.item_id = item_id
         factcheck.factchecking_organization_id = organization.id
         try:
             operations.update_object_db(factcheck)
@@ -114,7 +121,67 @@ def store_itemurl(event, context):
     Parameters
     ----------
     event: dict, required
-        factchecks
+        item
+        Claim
+
+        Event doc: https://docs.aws.amazon.com/apigateway/latest/developerguide/set-up-lambda-proxy-integrations.html#api-gateway-simple-proxy-for-lambda-input-format
+
+    context: object, required
+        Lambda Context runtime methods and attributes
+
+        Context doc: https://docs.aws.amazon.com/lambda/latest/dg/python-context-object.html
+
+    Returns
+    ------
+    API Gateway Lambda Proxy Output Format: application/json
+
+        Return doc: https://docs.aws.amazon.com/apigateway/latest/developerguide/set-up-lambda-proxy-integrations.html
+    """
+
+    print(event)
+
+    # Store all urls referenced in the item
+    for str_url in event['Claim']['urls']:
+        if str_url == "":
+            continue
+        # search for url in database
+        url = URL()
+        try:
+            url = operations.get_url_by_content_db(str_url)
+        except Exception:
+            # store url in database
+            url.id = str(uuid4())
+            url.url = str_url
+            try:
+                operations.update_object_db(url)
+            except Exception as e:
+                logger.error("Could not store urls. Exception: %s", e, exc_info=True)
+                raise
+        itemurl = ItemURL()
+        # itemurl already exists?
+        item_id = event['item']['id']
+        try:
+            itemurl = operations.get_itemurl_by_url_and_item_db(url.id, item_id)
+        except Exception:
+            # store itemurl in database
+            itemurl.id = str(uuid4())
+            itemurl.item_id = item_id
+            itemurl.url_id = url.id
+            try:
+                operations.update_object_db(itemurl)
+            except Exception as e:
+                logger.error("Could not store itemurls. Exception: %s", e, exc_info=True)
+                raise
+
+
+def store_itementity(event, context):
+    """stores entities of an item
+
+    Parameters
+    ----------
+    event: dict, required
+        item
+        Entities
 
         Event doc: https://docs.aws.amazon.com/apigateway/latest/developerguide/set-up-lambda-proxy-integrations.html#api-gateway-simple-proxy-for-lambda-input-format
 
@@ -135,7 +202,7 @@ def store_itemurl(event, context):
     # Store all urls referenced in the item
     url = URL()
     for str_url in event['Claim']['urls']:
-        if str_url=="":
+        if str_url == "":
             continue
         # search for url in database
         try:
