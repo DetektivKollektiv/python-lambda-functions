35f07213ab48b9e22a33e0dbed6e09dc7f17c317
Patrick Mueller <patrick96mueller@gmail.com>
2020-12-17 11:28:27 +0100

    One more try

--- lambda_layers/core_layer/python/core_layer/alembic/env.py
+++ lambda_layers/core_layer/python/core_layer/alembic/env.py
@@ -10,8 +10,8 @@ import os
 import json
 from os.path import abspath, dirname
 sys.path.insert(0, dirname(dirname(abspath(__file__))))
-from core_layer import helper
-from core_layer.model import model_base
+from .. import helper
+from ..model import model_base
 
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
