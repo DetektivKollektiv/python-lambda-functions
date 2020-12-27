2a5c26dbf437955330a21e672d47ce29d58f4d95
ML-Dirk <mail@drwacker.de>
2020-12-27 20:17:30 +0100

    take textualRating as title, if there is no title

--- lambda_functions/ml_service/SearchFactChecks.py
+++ lambda_functions/ml_service/SearchFactChecks.py
@@ -217,6 +217,13 @@ def get_FactChecks(event, context):
     loop = asyncio.get_event_loop()
     factcheck = loop.run_until_complete(
         get_article(search_terms, LanguageCode))
+
+    # check if there is a title of the factcheck, if not, then take the textualRating
+    if 'claimReview' in factcheck:
+        if 'title' not in factcheck['claimReview'][0]:
+            if 'textualRating' in factcheck['claimReview'][0]:
+                factcheck['claimReview'][0]['title'] = factcheck['claimReview'][0]['textualRating']
+
     claims.append(factcheck)
 
     return claims
--- lambda_functions/ml_service/tests/unit/test_SearchFactChecks.py
+++ lambda_functions/ml_service/tests/unit/test_SearchFactChecks.py
@@ -38,7 +38,7 @@ class TestSearchFactChecks:
         ret = SearchFactChecks.get_FactChecks(event, context)
         elapsed = time.perf_counter() - s
         assert 'claimReview' in ret[0]
-        assert ret[0]['claimReview'][0]['textualRating'] == 'Falsch. Das Robert-Koch-Institut bestätigte nicht eine Covid-19- Sterblichkeitsrate von 0,01 Prozent in Deutschland.'
+        assert ret[0]['claimReview'][0]['title'] == 'Falsch. Das Robert-Koch-Institut bestätigte nicht eine Covid-19- Sterblichkeitsrate von 0,01 Prozent in Deutschland.'
         assert elapsed < 3
 
     def test_search_checks_2(self):
@@ -62,7 +62,35 @@ class TestSearchFactChecks:
         elapsed = time.perf_counter() - s
         assert 'claimReview' in ret[0]
         assert ret[0]['claimReview'][0]['url'] == 'https://www.br.de/nachrichten/wissen/warum-viele-corona-tests-noch-keine-qualitaetskontrolle-brauchen,SI1KrNC'
-        assert ret[0]['claimReview'][0]['textualRating'] == 'Richtig. Corona-Tests, die für medizinisches Fachpersonal gedacht sind, müssen nicht durch eine externe Prüforganisation (Benannte Stelle) kontrolliert werden. Corona-Test zur Selbstanwendung hingegen schon. Ab 2022 müssen alle In-Vitro-Diagnostika, zu denen auch Corona-Tests zählen, von einer Benannte Stelle überprüft werden.'
+        assert ret[0]['claimReview'][0]['title'] == 'Richtig. Corona-Tests, die für medizinisches Fachpersonal gedacht sind, müssen nicht durch eine externe Prüforganisation (Benannte Stelle) kontrolliert werden. Corona-Test zur Selbstanwendung hingegen schon. Ab 2022 müssen alle In-Vitro-Diagnostika, zu denen auch Corona-Tests zählen, von einer Benannte Stelle überprüft werden.'
+        assert elapsed < 3
+
+    def test_search_checks_3(self):
+        os.environ["STAGE"] = "dev"
+        event = {
+            "item": {
+                "id": "3fb83912-7a97-423a-b820-36718d51b1a6",
+                "content": "Obama finanzierte Labor in Wuhan. | Viral Virus - Lass es jeden wissen",
+                "language": "de"
+            },
+            "KeyPhrases": [
+                "Obama",
+                "Labor",
+                "jeden"
+            ],
+            "Entities": [
+                "Wuhan",
+                "Obama",
+                "jeden"
+            ]
+        }
+        context = ""
+        s = time.perf_counter()
+        ret = SearchFactChecks.get_FactChecks(event, context)
+        elapsed = time.perf_counter() - s
+        assert 'claimReview' in ret[0]
+        assert ret[0]['claimReview'][0]['url'] == 'https://correctiv.org/faktencheck/2020/07/21/nein-die-us-regierung-unter-obama-hat-das-institut-fuer-virologie-in-wuhan-nicht-mit-37-millionen-dollar-unterstuetzt/'
+        assert ret[0]['claimReview'][0]['title'] == 'Größtenteils falsch. Obama hat das Labor in Wuhan nicht mit 3,7 Millionen US-Dollar unterstützt.'
         assert elapsed < 3
 
 class TestStepfunction:
