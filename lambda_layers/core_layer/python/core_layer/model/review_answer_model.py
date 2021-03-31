84e80c07f909d744a1b5af48b05ce95b17e84109
PotSchmoell <patrick96mueller@gmail.com>
2021-03-31 09:34:45 +0200

    - Added ondelete and onupdate behavior for foreign keys in question option pairs - Moved question option pairs to question model file - Added migration

--- lambda_layers/core_layer/python/core_layer/model/review_answer_model.py
+++ lambda_layers/core_layer/python/core_layer/model/review_answer_model.py
@@ -2,6 +2,7 @@ from sqlalchemy import Table, Column, DateTime, String, Integer, ForeignKey, fun
 from sqlalchemy.orm import relationship
 from .model_base import Base
 
+from core_layer.model.review_question_model import question_option_pairs
 
 class ReviewAnswer(Base):
     __tablename__ = 'review_answers'
@@ -21,15 +22,6 @@ class ReviewAnswer(Base):
     def to_dict(self):
         return {"id": self.id, "review_id": self.review_id, "review_question_id": self.review_question_id,
                 "answer": self.answer, "comment": self.comment}
-
-
-question_option_pairs = Table('question_option_pairs', Base.metadata,
-                              Column('question_id', String(36),
-                                     ForeignKey('review_questions.id')),
-                              Column('option_id', String(36),
-                                     ForeignKey('answer_options.id'))
-                              )
-
 
 class AnswerOption(Base):
     __tablename__ = 'answer_options'
