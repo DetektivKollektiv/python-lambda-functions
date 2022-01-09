1bc71e17bf258983835b5fc07172c25a177c8054
PotSchmoell <patrick96mueller@gmail.com>
2022-01-09 12:53:52 +0100

    - Changed query for items to ignore query param in incoming url and database - Added function in db_handler to get substring from db column for sqlite and mysql

--- lambda_layers/core_layer/python/core_layer/db_handler.py
+++ lambda_layers/core_layer/python/core_layer/db_handler.py
@@ -2,13 +2,14 @@ import logging
 import sqlite3
 import os
 
-from sqlalchemy import create_engine
+from sqlalchemy import create_engine, func
 from sqlalchemy.orm import sessionmaker, scoped_session
+from sqlalchemy.sql.expression import case
 from .model.model_base import Base
 
 """Initializes a Database Session."""
 
-logger = logging.getLogger() 
+logger = logging.getLogger()
 logger.setLevel(logging.INFO)
 
 logger.info('New DB Session initiated')
@@ -19,13 +20,12 @@ secret_arn = "arn:aws:secretsmanager:eu-central-1:891514678401:secret:Serverless
 if 'DBNAME' in os.environ:
     database_name = os.environ['DBNAME']
     db = create_engine('mysql+auroradataapi://:@/{0}'.format(database_name),
-                        echo=True,
-                        connect_args=dict(aurora_cluster_arn=cluster_arn, secret_arn=secret_arn))
+                       echo=True,
+                       connect_args=dict(aurora_cluster_arn=cluster_arn, secret_arn=secret_arn))
 else:
     def creator(): return sqlite3.connect(
-            'file::memory:?cache=shared', uri=True, check_same_thread=False)
+        'file::memory:?cache=shared', uri=True, check_same_thread=False)
     db = create_engine('sqlite://', creator=creator)
-
 
     Base.metadata.create_all(db)
     db.execute('pragma foreign_keys=on')
@@ -33,6 +33,14 @@ else:
 
 session_factory = sessionmaker(bind=db, expire_on_commit=False, autoflush=True)
 Session = scoped_session(session_factory)
+
+
+def substring(column, delimeter, session):
+    if session.bind.dialect.name == 'sqlite':
+        return func.iif(func.instr(column, delimeter) > 0, func.substr(
+            column, 1, func.instr(column, delimeter)-1), column)
+    elif session.bind.dialect.name == 'mysql+auroradataapi':
+        return func.substring_index(column, delimeter, 1)
 
 
 def update_object(obj, session):
