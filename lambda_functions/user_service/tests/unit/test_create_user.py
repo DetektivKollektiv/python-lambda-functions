5c279aba074a99277f655e1d9672edd40d4871f5
51moon <g51moon@t-online.de>
2022-03-12 20:25:02 +0100

    apply requested adaptions

--- lambda_functions/user_service/tests/unit/test_create_user.py
+++ lambda_functions/user_service/tests/unit/test_create_user.py
@@ -22,7 +22,7 @@ def mail_address():
     return "mail@provider.com"
 
 @pytest.fixture
-def event(user_id, user_name, mail_address):
+def event_mail_subscription_0(user_id, user_name, mail_address):
     event = {
              "triggerSource": "PostConfirmation_ConfirmSignUp",
              "userName": user_name,
@@ -31,7 +31,38 @@ def event(user_id, user_name, mail_address):
                      "userAttributes": {
                          "email": mail_address,
                          "sub": user_id,
-                         "custom:mail_subscription": 0
+                         "custom:mail_subscription": '0'
+                     }
+                 }
+            }
+    return event
+
+@pytest.fixture
+def event_mail_subscription_1():
+    event = {
+             "triggerSource": "PostConfirmation_ConfirmSignUp",
+             "userName": "user_name2",
+             "request":
+                 {
+                     "userAttributes": {
+                         "email": "mail2@provider.com",
+                         "sub": str(uuid4()),
+                         "custom:mail_subscription": '1'
+                     }
+                 }
+            }
+    return event
+
+@pytest.fixture
+def event_without_mail_subscription_status():
+    event = {
+             "triggerSource": "PostConfirmation_ConfirmSignUp",
+             "userName": "user_name3",
+             "request":
+                 {
+                     "userAttributes": {
+                         "email": "mail3@provider.com",
+                         "sub": str(uuid4())
                      }
                  }
             }
@@ -40,7 +71,7 @@ def event(user_id, user_name, mail_address):
 
 @mock_ses
 @mock_cognitoidp
-def test_create_user(event, user_name, mail_address, monkeypatch):
+def test_create_user(event_mail_subscription_0, event_mail_subscription_1, event_without_mail_subscription_status, user_name, mail_address, monkeypatch):
 
 
     # mock required stuff
@@ -52,28 +83,38 @@ def test_create_user(event, user_name, mail_address, monkeypatch):
 
     cognitoidp = boto3.client("cognito-idp", region_name = "eu-central-1")
 
-    response = cognitoidp.create_user_pool(PoolName = "PoolNameString")
-    user_pool_id = response['UserPool']['Id']
-    event['userPoolId'] = user_pool_id # add user_pool_id to event as it's requested from there by admin_add_user_to_group method
-
-    cognitoidp.create_group(GroupName = 'Detective',
-                            UserPoolId = user_pool_id
-                            )
-    cognitoidp.admin_create_user(UserPoolId = user_pool_id,
-                           Username = user_name
-                           )
+    # add user_pool_id to event as it's requested from there by admin_add_user_to_group method
+    for event in [event_mail_subscription_0, event_mail_subscription_1, event_without_mail_subscription_status]:
+        response = cognitoidp.create_user_pool(PoolName = "PoolNameString")
+        user_pool_id = response['UserPool']['Id']
+        event['userPoolId'] = user_pool_id 
+        cognitoidp.create_group(GroupName = 'Detective',
+                                UserPoolId = user_pool_id
+                                )
+        cognitoidp.admin_create_user(UserPoolId = user_pool_id,
+                                     Username = event['userName']
+                                     )
 
 
     with Session() as session:
 
         level_1_obj = Level(id = 1)
         session.add_all([level_1_obj])
-        session.commit()    
-        create_user(event)
+        session.commit()
+        create_user(event_mail_subscription_0)
 
         # Check if user and mail is created
         user = session.query(User).first()
         assert user.name == user_name
         assert user.email.email == mail_address
         mail = session.query(Mail).first()
+        assert mail.status == 'unsubscribed'
+
+        # Check cases with other "custom:mail_subscription" attributes
+        create_user(event_mail_subscription_1)
+        mail = session.query(Mail).all()[1]
+        assert mail.status == 'confirmed'
+
+        create_user(event_without_mail_subscription_status)
+        mail = session.query(Mail).all()[2]
         assert mail.status == 'unsubscribed'
